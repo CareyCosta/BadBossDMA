041cbb5774276573a216739ea3f32bca96bef6f7
Carey Costa <careyxcosta@gmail.com>
2017-02-02 13:52:16 -0500

    rename old table to new table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202181325) do
+ActiveRecord::Schema.define(version: 20170202185046) do
 
   create_table "locations", force: :cascade do |t|
     t.string "city"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20170202181325) do
     t.integer "quantity"
   end
 
-  create_table "party_guests", force: :cascade do |t|
+  create_table "widgets", force: :cascade do |t|
     t.string  "first_name"
     t.string  "last_name"
     t.string  "dietary_restrictions"
